c860d69773801d3aa081cc73d669c724df1fb256
shashank-adsule <da25m005@smail.iitm.ac.in>
2026-03-05 18:46:49 +0530

    Delete src directory

--- src/src/ann/__init__.py
+++
@@ -1,4 +0,0 @@
-from .activation import get_activation, ReLU, Sigmoid, Tanh, Softmax
-from .losses     import get_loss, CrossEntropyLoss, MSELoss
-from .layers     import Linear, init_weights
-from .optimizers import get_optimizer, SGD, Momentum, NAG, RMSProp, Adam, Nadam
